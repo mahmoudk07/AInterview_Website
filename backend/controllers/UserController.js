d6fdb8597c19a233b1cac9fcbad79d2de1286e4f
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-08 01:56:46 +0200

    🐛 Fix bug in Login logic

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,27 +1,27 @@
 import { UserModel } from "../models/UserModel.js"
 import { StatusCodes } from 'http-status-codes'
 import { AsyncHandler } from "../middlewares/AsyncHandler.js"
+import { BadRequest } from "../errors/BadRequestError.js";
+import { UnAuthentication } from "../errors/UnAuthenticationError.js";
+import { NotFound } from "../errors/NotFoundError.js";
 import bcrypt from "bcrypt";
 export const Signup = AsyncHandler(async (request, response) => {
-    const { firstname, lastname, email, password, role } = request.body
-    if (!firstname || !lastname || !email || !password || !role)
-        return response.status(StatusCodes.UNAUTHORIZED).json({
-            success: false,
-            message: "Please provide all fields"
-        })
+    const { firstname, lastname, email, password, role, job } = request.body
+    if (!firstname || !lastname || !email || !password || !role || !job)
+        throw new UnAuthentication(`All fields are required`);
     const user = await UserModel.create(request.body)
     return response.status(StatusCodes.OK).json(user)
 })
 export const Login = AsyncHandler(async (request, response) => {
     const { email, password } = request.body
     if (!email || !password)
-        return response.status(StatusCodes.UNAUTHORIZED).json({ success: false, message: "Please provide email and password" })
+        throw new UnAuthentication(`Please provide both email and password`)
     const user = await UserModel.findOne({ email: email })
     if (!user)
-        return response.status(StatusCodes.UNAUTHORIZED).json(`No user with email: ${email}`)
+        throw new NotFound(`No user with email: ${email}`)
     const isPasswordMatched = await bcrypt.compare(password, user.password)
     if (!isPasswordMatched)
-        return response.status(StatusCodes.UNAUTHORIZED).json(`Please provide a correct password to login`)
+        throw new UnAuthentication(`Please provide a correct password to login`)
     const token = user.CreateJWT()
     return response.status(StatusCodes.OK).json({
         message: `User logged in successfully`,
@@ -39,7 +39,7 @@ export const getUserByID = AsyncHandler(async (request, response) => {
     const { id } = request.params
     const user = await UserModel.findOne({ _id: id })
     if (!user)
-        return response.status(StatusCodes.NOT_FOUND).json(`No user with ID: ${id}`)
+        throw new NotFound(`No user with ID: ${id}`)
     return response.status(StatusCodes.OK).json(user)
 })
 export const deleteAllUsers = AsyncHandler(async (request, response) => {
@@ -50,14 +50,14 @@ export const deleteUserByID = AsyncHandler(async (request, response) => {
     const { id } = request.params
     const user = await UserModel.findOne({ _id: id })
     if (!user)
-        return response.status(StatusCodes.NOT_FOUND).json(`No user with ID: ${id}`)
+        throw new NotFound(`No user with ID: ${id}`)
     await UserModel.deleteOne({ _id: id })
     return response.status(StatusCodes.OK).json(`User with ID: ${id} deleted successfully`)
 })
 export const UpdatePassword = AsyncHandler(async (request, response) => {
     const { newPassword } = request.body
     if (!newPassword)
-        return response.status(StatusCodes.BAD_REQUEST).json(`Please provide new password to update old password`)
+        throw new BadRequest(`Please provide a new password`)
     const { id } = request.user
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(newPassword, salt)
