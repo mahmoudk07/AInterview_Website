71c5c8c48c36590d452158d16359cb09d2a0056d
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-08 01:55:35 +0200

    ✅ Modify returned error response

--- frontend/src/services/auth/authSlice.js
+++ frontend/src/services/auth/authSlice.js
@@ -8,7 +8,7 @@ export const SignupUser = createAsyncThunk('user/signup', async (data, thunkAPI)
         return response.data
     }
     catch (error) {
-        return rejectWithValue(error.response)
+        return rejectWithValue(error.response.data)
     }
 })
 export const LoginUser = createAsyncThunk('user/login', async (data, thunkAPI) => {
@@ -18,7 +18,7 @@ export const LoginUser = createAsyncThunk('user/login', async (data, thunkAPI) =
         return response.data
     }
     catch (error) {
-        return rejectWithValue(error.response)
+        return rejectWithValue(error.response.data)
     }
 })
 
@@ -37,14 +37,15 @@ const userSlice = createSlice({
         logout: (state , action) => {
             state.isLoggedIn = false
             localStorage.removeItem('token')
-        }
+        },
     },
     extraReducers: (builder) => {
         builder.addCase(SignupUser.pending, (state, action) => {
             state.isLoading = true
         })
         builder.addCase(SignupUser.fulfilled, (state, action) => {
-          state.isLoading = false;
+            state.isLoading = false;
+            state.error = null
         });
         builder.addCase(SignupUser.rejected, (state, action) => {
             state.isLoading = false
@@ -59,6 +60,7 @@ const userSlice = createSlice({
             state.token = action.payload.token
             state.isLoggedIn = true
             state.isLoading = false
+            state.error = null
             localStorage.setItem('token', action.payload.token)
             localStorage.setItem('firstname', action.payload.firstname)
             localStorage.setItem('lastname', action.payload.lastname)
