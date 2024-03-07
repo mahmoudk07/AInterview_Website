1febca5fa99e9c15656fda7d58d87a15f65ff061
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-07 18:50:23 +0200

    ✅ Add change password router

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { Login, Signup, getAllUsers, getUserByID, deleteAllUsers, deleteUserByID } from "../controllers/UserController.js"
+import { Login, Signup, getAllUsers, getUserByID, deleteAllUsers, deleteUserByID, UpdatePassword } from "../controllers/UserController.js"
 import { IsAdmin } from "../middlewares/IsAdmin.js"
 import { IsAuthorized } from "../middlewares/IsAuthorized.js"
 export const router = Router()
@@ -9,4 +9,5 @@ router.post('/signup', Signup)
 router.get('/getAllUsers', IsAuthorized , IsAdmin , getAllUsers)
 router.get('/getUser/:id', IsAuthorized , IsAdmin , getUserByID)
 router.delete('/deleteAllUsers', IsAuthorized , IsAdmin , deleteAllUsers)
-router.delete('/deleteUser/:id', IsAuthorized , IsAdmin , deleteUserByID)
+router.delete('/deleteUser/:id', IsAuthorized, IsAdmin, deleteUserByID)
+router.patch('/changePassword', IsAuthorized , UpdatePassword)
