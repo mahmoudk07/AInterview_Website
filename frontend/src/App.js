226994ace81f3783b84552cc1c3564811532ec27
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-07 23:11:41 +0200

    ✅ Add Home path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Layout from './app/Layout'
 import Signup from './pages/Signup/Signup';
+import Home from './pages/Home/Home';
 import { Provider } from "react-redux";
 import { store } from './store';
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Provider store={store}>
           <BrowserRouter>
             <Routes>
+              <Route path = '/' element = {<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
             </Routes>
