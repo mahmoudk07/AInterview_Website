6a6d68080f3ad89229335f7d0fa84df114a17dbc
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-07 20:48:59 +0200

    ⚡️ Wrap app in Provider component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Layout from './app/Layout'
 import Signup from './pages/Signup/Signup';
+import { Provider } from "react-redux";
+import { store } from './store';
 function App() {
   return (
     <>
       <Layout>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </BrowserRouter>
+        </Provider>
       </Layout>
     </>
   );
