b3f0e8ba46c5dd19ea5dd8cd2e1cb5ca9e4d9160
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-07 23:11:55 +0200

    ✅ Add utilities functions

--- frontend/src/services/auth/authSlice.js
+++ frontend/src/services/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-export const Signup = createAsyncThunk('user/signup', async (data, thunkAPI) => {
+export const SignupUser = createAsyncThunk('user/signup', async (data, thunkAPI) => {
     const { rejectWithValue } = thunkAPI;
     try {
         const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/signup`, data)
@@ -11,7 +11,7 @@ export const Signup = createAsyncThunk('user/signup', async (data, thunkAPI) => 
         return rejectWithValue(error.response)
     }
 })
-export const Login = createAsyncThunk('user/login', async (data, thunkAPI) => {
+export const LoginUser = createAsyncThunk('user/login', async (data, thunkAPI) => {
     const { rejectWithValue } = thunkAPI
     try {
         const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login`, data)
@@ -40,20 +40,20 @@ const userSlice = createSlice({
         }
     },
     extraReducers: (builder) => {
-        builder.addCase(Signup.pending, (state, action) => {
+        builder.addCase(SignupUser.pending, (state, action) => {
             state.isLoading = true
         })
-        builder.addCase(Signup.fulfilled, (state, action) => {
+        builder.addCase(SignupUser.fulfilled, (state, action) => {
           state.isLoading = false;
         });
-        builder.addCase(Signup.rejected, (state, action) => {
+        builder.addCase(SignupUser.rejected, (state, action) => {
             state.isLoading = false
             state.error = action.payload
         })
-        builder.addCase(Login.pending, (state, action) => {
+        builder.addCase(LoginUser.pending, (state, action) => {
             state.isLoading = true
         })
-        builder.addCase(Login.fulfilled, (state, action) => {
+        builder.addCase(LoginUser.fulfilled, (state, action) => {
             state.firstname = action.payload.firstname
             state.lastname = action.payload.lastname
             state.token = action.payload.token
@@ -63,11 +63,12 @@ const userSlice = createSlice({
             localStorage.setItem('firstname', action.payload.firstname)
             localStorage.setItem('lastname', action.payload.lastname)
         })
-        builder.addCase(Login.rejected, (state, action) => {
+        builder.addCase(LoginUser.rejected, (state, action) => {
             state.isLoading = false
             state.error = action.payload
         })
     }
 })
+export const Error = (state) => state.User.error
 export const { logout } = userSlice.actions
 export default userSlice.reducer
