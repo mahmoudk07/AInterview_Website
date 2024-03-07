1b7e9f25ae6a79a4310299da67ad8e287897eaaf
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-07 18:04:41 +0200

    ✅ Add notFound middleware in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import morgan from 'morgan'
 import { database_connection } from './config/DB_connection.js'
+import { router } from './routes/UserRouter.js'
+import { NotFoundMiddleware } from './middlewares/NotFound.js'
 dotenv.config()
 const app = express()
 
@@ -10,6 +12,10 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use('/api/auth', router)
+
+app.use(NotFoundMiddleware)
 
 const start = async () => {
     await database_connection(process.env.DB_CONNECTION);
