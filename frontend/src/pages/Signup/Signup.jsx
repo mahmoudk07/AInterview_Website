5827582f41f5dc5ac3e7a845e93b1724e42341c5
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-11 01:28:47 +0200

    🔥 Clean Signup code

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,28 +1,32 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch , useSelector } from 'react-redux';
 import { SignupUser } from '../../services/auth/authSlice';
 import { useNavigate } from 'react-router-dom';
+import Modal from '../../components/Modal/Modal';
 import './Signup.css'
 const Signup = () => {
-    const [name , setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [gender, setGender] = useState("")
-    const [job, setJob] = useState("Software Engineer")
-    const [role, setRole] = useState("User")
+    const [data, setData] = useState({
+        firstname: "", lastname: "" , email: "" , password: "" , gender: "", job: "Software Engineer", role: "user"
+    })
+    const [showModal, setShowModal] = useState(false)
+    const { error } = useSelector((state) => state.User)
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const data = {
-        firstname: name.split(' ')[0], lastname: name.split(' ')[1],
-        email, password, gender, job, role: role.toLowerCase()
-    }
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (name !== "" && email !== "" && password !== "" && job !== "" && role !== "")
+        if (data.firstname !== "" && data.email !== "" && data.password !== "" && data.job !== "" && data.role !== "")
             dispatch(SignupUser(data)).then((response) => {
                 if (!response.hasOwnProperty('error'))
                     navigate('/login')
+                else
+                    openModal()
             })
+    }
+    const openModal = () => { 
+        setShowModal(true)
+    }
+    const closeModal = () => { 
+        setShowModal(false)
     }
     const jobTitles = [
         'Data Scientist',
@@ -37,23 +41,24 @@ const Signup = () => {
     ];
     return (
     <div className = 'flex items-center justify-center w-[100vw] h-[100vh]'>
-        <form className = 'login__form min-w-[25%] h-[88%] bg-transparent py-[30px] px-[40px]' onSubmit = {handleSubmit}>
+        <Modal show = { showModal } message = { error } close = { closeModal } />
+        <form className = 'login__form min-w-[25%] min-h-[80%] bg-transparent py-[30px] px-[40px]' onSubmit = {handleSubmit}>
               <h1 className='text-3xl font-bold text-white text-center mb-[35px]'>Signup</h1>
               <div className = 'relative'>
                   <input className='bg-transparent w-full p-[10px] text-white rounded-[20px] outline-none border-[2px] border-solid border-gray-500 mb-[35px] pr-[40px]'
-                      type='text' id='name' placeholder='Name' required onChange = {(e) => setName(e.target.value)}
+                      type='text' id='name' placeholder='Name' required onChange = {(e) => setData({...data , firstname: e.target.value.split(' ')[0], lastname: e.target.value.split(' ')[1]})}
                   />
                   <i className='absolute right-[15px] top-[18%] text-[20px] bx bxs-user'></i>
                 </div>
                 <div className='relative'>
                     <input className='bg-transparent w-full p-[10px] text-white rounded-[20px] outline-none border-[2px] border-solid border-gray-500 mb-[35px] pr-[40px]'
-                        type='email' id='email' placeholder='Email' required onChange = {(e) => setEmail(e.target.value)}
+                        type='email' id='email' placeholder='Email' required onChange = {(e) => setData({...data , email: e.target.value})}
                     />
                     <i className='absolute right-[15px] top-[18%] text-[20px] bx bxs-envelope'></i>
                 </div>
               <div className = 'relative'>
-                  <input className='bg-transparent w-full p-[10px] text-white rounded-[20px] outline-none border-[2px] border-solid border-gray-500 mb-[35px] pr-[40px]'
-                      type='password' id='password' placeholder='Password' required onChange = {(e) => setPassword(e.target.value)}
+                    <input className='bg-transparent w-full p-[10px] text-white rounded-[20px] outline-none border-[2px] border-solid border-gray-500 mb-[35px] pr-[40px]'
+                        type='password' id='password' placeholder='Password' required onChange={(e) => setData({ ...data, password: e.target.value })}
                   />
                   <i className = 'absolute right-[15px] top-[17px] text-[20px] bx bxs-lock-alt'></i>
                 </div>
@@ -66,7 +71,7 @@ const Signup = () => {
                                 value="male"
                                 className='mr-[5px]'
                                 id='male'
-                                onChange = {(e) => setGender(e.target.value)}
+                                onChange = {(e) => setData({...data , gender: e.target.value})}
                             />
                             Male
                         </label>
@@ -77,12 +82,12 @@ const Signup = () => {
                                 value="female"
                                 className='mr-[5px]'
                                 id='female'
-                                onChange = {(e) => setGender(e.target.value)}
+                                onChange = {(e) => setData({...data , gender : e.target.value})}
                             />
                             Female
                         </label>
                     </div>
-                    <select className= 'bg-transparent rounded-[10px] outline-none border-[2px] border-solid border-gray-500 text-gray-300 appearance-none font-bold px-[8px] py-[7px] cursor-pointer' id='role' onChange = {(e) => setRole(e.target.value)}>
+                    <select className= 'bg-transparent rounded-[10px] outline-none border-[2px] border-solid border-gray-500 text-gray-300 appearance-none font-bold px-[8px] py-[7px] cursor-pointer' id='role' onChange = {(e) => setData({...data , role : e.target.value.toLowerCase()})}>
                         <option value = 'user'>User</option>
                         <option value = 'admin'>Admin</option>
                         <option value = 'manager'>Manager</option>
@@ -90,7 +95,7 @@ const Signup = () => {
                 </div>
                 <div className="job__title__selector mb-[25px]">
                     <label className = 'text-gray-300 font-bold block' htmlFor="job-title">Select a job title:</label>
-                    <select className= 'mt-[5px] p-[10px] rounded-[10px] font-bold appearance-none bg-transparent border-[2px] border-solid border-gray-500 text-gray-300 outline-none cursor-pointer' id = "job-title" onChange = {(e) => setJob(e.target.value)}>
+                    <select className= 'mt-[5px] p-[10px] rounded-[10px] font-bold appearance-none bg-transparent border-[2px] border-solid border-gray-500 text-gray-300 outline-none cursor-pointer' id = "job-title" onChange = {(e) => setData({...data , job : e.target.value})}>
                         <option value = "Software Engineer">Software Engineer</option>
                         {jobTitles.map((title) => (
                             <option key={title} value={title}>
