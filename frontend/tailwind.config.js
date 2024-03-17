81013272c8e1ded363f71994c2f4c2caf261ad2b
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-17 03:34:42 +0200

    ✅ Add custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "rgba(0,21,52,255",
+        "secondary-color": "#98C1D9",
+        "tertiary-color": "#E0FBFC",
+        "quaternary-color": "#293241",
+      },
+    },
   },
   plugins: [],
 };
