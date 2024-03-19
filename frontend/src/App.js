ae58487e92d60681f6036e831c18d39ac06922bd
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-19 17:28:49 +0200

    ✅ Add more routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Layout from './app/Layout'
 import Signup from './pages/Interviewee/Signup/Signup';
 import Home from './pages/Company/Home/Home';
 import Profile from './pages/Company/Profile/Profile';
+import CreateInterview from './pages/Company/CreateInterview/CreateInterview';
 import { Provider } from "react-redux";
 import { store } from './store';
 function App() {
@@ -17,6 +18,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/interview" element={<CreateInterview />} />
             </Routes>
           </BrowserRouter>
         </Provider>
