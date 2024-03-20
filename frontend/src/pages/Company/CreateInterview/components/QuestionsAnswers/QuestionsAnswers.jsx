d0b9b01ab77dfdeb71fa273b7b9a66620db337e3
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-20 03:24:54 +0200

    ✅ Add Questions and Answers Components

--- frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { Input } from '@material-tailwind/react'
 const QuestionsAnswers = () => {
+    const data = [1 , 2 , 3 , 4 , 5]
   return (
-    <div>QuestionsAnswers</div>
+    <div className = 'mt-[60px]'>
+        {
+            data.map((item, index) => (
+                <div className='flex-col space-y-2 mb-[20px] bg-transparent p-[3%] border-[2px] border-gray-600 rounded-[10px]'>
+                    <Input type="text" label={`Question ${index + 1}`} variant='outlined' color='white' />
+                    <Input type="text" label="Answer" variant='outlined' color='white' />
+                </div>
+            ))
+        }
+    </div>
   )
 }
-
 export default QuestionsAnswers
