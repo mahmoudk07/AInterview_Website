df5a4650d2c1dba6f5f85a56c70c677e62d98e25
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-20 17:59:09 +0200

    ✅ check for final data

--- frontend/src/pages/Company/CreateInterview/components/CompletedInterview/CompletedInterview.jsx
+++ frontend/src/pages/Company/CreateInterview/components/CompletedInterview/CompletedInterview.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+// import { InterviewContext } from '../../CreateInterview'
 const CompletedInterview = () => {
+  // const { data, setDate } = useContext(InterviewContext) 
     const navigate = useNavigate()
     useEffect(() => {
         setTimeout(() => navigate("/") , 2500)
     }, [navigate])
+  // console.log(data)
   return (
     <div className = 'mt-[60px]'>
           <div className='flex-col text-center'>
