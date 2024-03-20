d27155355ea236789fd445bbc3d6a5e1d2b7688f
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-20 17:58:25 +0200

    ✅ Add Validation to Questions and Answers form

--- frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Input } from '@material-tailwind/react'
+import { InterviewContext } from '../../CreateInterview'
 const QuestionsAnswers = () => {
-    const data = [1 , 2 , 3 , 4 , 5]
+    const { data, setData } = useContext(InterviewContext)
+    const addQuestion = (e , index) => {
+        const newQuestions = [...data.Questions]
+        newQuestions[index][`Q${index + 1}`] = e.target.value
+        setData({...data , Questions: newQuestions})
+    }
+    const addAnswer = (e, index) => {
+        const newAnswer = [...data.Questions]
+        newAnswer[index]['Answer'] = e.target.value
+        setData({...data , Questions: newAnswer})
+    }
   return (
     <div className = 'mt-[60px]'>
         {
-            data.map((item, index) => (
-                <div className='flex-col space-y-2 mb-[20px] bg-transparent p-[3%] border-[2px] border-gray-600 rounded-[10px]'>
-                    <Input type="text" label={`Question ${index + 1}`} variant='outlined' color='white' />
-                    <Input type="text" label="Answer" variant='outlined' color='white' />
+            data.Questions.map((question, index) => (
+                <div key = {index} className='flex-col space-y-2 mb-[20px] bg-transparent p-[3%] border-[2px] border-gray-600 rounded-[10px]'>
+                    <Input type="text" label={`Question ${index + 1}`} variant='outlined' color='white' onChange={(e) => addQuestion(e , index)}/>
+                    <Input type="text" label="Answer" variant='outlined' color='white' onChange = {(e) => addAnswer(e, index)}/>
                 </div>
             ))
         }
