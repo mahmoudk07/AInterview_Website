2799a8cb79b72224b9cf53dbcaae959085c214ad
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-03-20 03:24:41 +0200

    ✅ Add Interview Scheduling Component

--- frontend/src/pages/Company/CreateInterview/components/Scheduling/InterviewScheduling.jsx
+++ frontend/src/pages/Company/CreateInterview/components/Scheduling/InterviewScheduling.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Input } from '@material-tailwind/react'
 const InterviewScheduling = () => {
   return (
-    <div>InterviewScheduling</div>
+    <div className = 'mt-[60px]'>
+        <div className = 'flex-col items-center space-y-5'>
+            <Input type = "text" label = "Title" variant = 'outlined' color = 'white'  />
+            <Input type = "text" label = "Date" variant = 'outlined' color = 'white'  />
+            <Input type = "text" label = "Time" variant = 'outlined' color = 'white' />
+            <Input type = "text" label = "Number of Questions" variant = 'outlined' color = 'white' />
+        </div>
+    </div>
   )
 }
-
 export default InterviewScheduling
