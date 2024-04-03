d3860f49966ab17ba495f4b1bad404c6cb64d060
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-03 04:26:12 +0200

    ✅ Add more request handlers

--- backend/controllers/UserController.py
+++ backend/controllers/UserController.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, status
+from datetime import timedelta
+from bson import ObjectId
+from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
-from schemas.userSchema import RegisterSchema, LoginSchema
+from schemas.userSchema import RegisterSchema, LoginSchema , PasswordSchema
 from models.user import User
 from services.userServices import UserServices
 from fastapi.responses import JSONResponse
@@ -35,4 +37,39 @@ async def loginUser(request: LoginSchema):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     if not UserServices.verify_password(request.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
-    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "User logged in successfully", "token": "token"})
+    token = UserServices.generate_JWT(user.email, timedelta(minutes=30))
+    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "User logged in successfully", "token": token})
+
+@UserRoutes.post("/changePassword" , summary = "Change Password of the user")
+async def changePassword(request: PasswordSchema , payload : dict = Depends(UserServices.is_authorized_user)):
+    email = payload.get("email")
+    user = await UserServices.get_user_by_email(email)
+    if not UserServices.verify_password(request.old_password, user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid old password")
+    if request.new_password != request.confirm_password:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Please enter two matching passwords")
+    user.password = UserServices.hashed_password(request.new_password)
+    try:
+        await user.save()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error occurred while saving")
+    return JSONResponse(status_code = status.HTTP_200_OK, content = {"message": "Password changed successfully"})
+
+@UserRoutes.get("/getAllUsers" , summary = "Get all signed up users")
+async def getAllUsers(user: dict = Depends(UserServices.is_admin_user)):
+    users =  await User.find().to_list()
+    users = [user.dict() for user in users]
+    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Users retrieved successfully", "users": users})
+
+@UserRoutes.delete("/deleteUser/{id}" , summary = "Delete user by id")
+async def deleteUser(id: str , user: dict = Depends(UserServices.is_admin_user)):
+    user = await User.find_one(User.id == ObjectId(id))
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    await user.delete()
+    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "User deleted successfully"})
+
+@UserRoutes.delete("/deleteAllUsers" , summary = "Delete all users")
+async def deleteAllUsers(user: dict = Depends(UserServices.is_admin_user)):
+    await User.delete_all()
+    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "All users deleted successfully"})
