134c9da599afa5387503fedf353a2b0478488ec4
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-14 00:48:21 +0200

    🔥 add admin routes

--- backend/routes/router.py
+++ backend/routes/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from controllers.UserController import UserRoutes
 from controllers.InterviewController import InterviewRoutes
 from controllers.CompanyController import CompanyRoutes
+from controllers.AdminController import AdminRoutes
 router = APIRouter()
 
-router.include_router(UserRoutes , prefix = '/auth' , tags = ["User Authnetication"])
+router.include_router(UserRoutes , prefix = '/auth' , tags = ["User APIs"])
 router.include_router(InterviewRoutes, prefix = '/interview' , tags = ["Interview APIs"])
 router.include_router(CompanyRoutes, prefix = "/company", tags = ["Company APIs"])
+router.include_router(AdminRoutes, prefix = "/admin", tags = ["Admin APIs"])
