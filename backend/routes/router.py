61ef6d37b81a32d797167d09d6cb59d5ab91080d
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-14 00:35:05 +0200

    ✅ add company routes

--- backend/routes/router.py
+++ backend/routes/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from controllers.UserController import UserRoutes
 from controllers.InterviewController import InterviewRoutes
+from controllers.CompanyController import CompanyRoutes
 router = APIRouter()
 
 router.include_router(UserRoutes , prefix = '/auth' , tags = ["User Authnetication"])
 router.include_router(InterviewRoutes, prefix = '/interview' , tags = ["Interview APIs"])
+router.include_router(CompanyRoutes, prefix = "/company", tags = ["Company APIs"])
