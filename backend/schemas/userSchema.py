6f0a0ab5e77a28dca731c709bd18b6676045d51a
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-13 18:58:28 +0200

    ✅ add updateUser shcema

--- backend/schemas/userSchema.py
+++ backend/schemas/userSchema.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+from bson import ObjectId
 from pydantic import BaseModel , Field , EmailStr
 
 class RegisterSchema(BaseModel):
@@ -18,6 +20,30 @@ class RegisterSchema(BaseModel):
           "Customer Service Representative",
           "Operations Manager"])
     role: str = Field(..., description = "Role of the user", enum = ['admin', 'user', 'manager'])
+class UpdateUser(BaseModel):
+    firstname: Optional[str] = Field(None, min_length = 3 , max_length = 20 , description = "First name of the user")
+    lastname: Optional[str] = Field(None , min_length = 3 , max_length = 20 , description = "Last name of the user")
+    email: Optional[EmailStr] = Field(None, description = "Email address of the user")
+    password: Optional[str] = Field(None, description = "Password of the user")
+    gender: Optional[str] = Field(None, description = "gender of the user")
+    job: Optional[str] = Field(None , description = "Job title of the user", enum = [
+          "Software Engineer",
+          "Data Scientist",
+          "Product Manager",
+          "UX Designer",
+          "Marketing Manager",
+          "Accountant",
+          "HR Specialist",
+          "Sales Representative",
+          "Customer Service Representative",
+          "Operations Manager"])
+    role: str = Field(None, description = "Role of the user", enum = ['admin', 'user', 'manager'])
+    interviews : Optional[List[ObjectId]] = Field([] , description = "Interviews of the user")
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: lambda oid: str(oid),  # Converts ObjectId to string for JSON serialization
+        }
 
 class LoginSchema(BaseModel):
     email: EmailStr = Field(... , description = 'Email address of the user')
