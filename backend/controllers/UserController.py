c73a9401f76d8395648217c499bf361efd7630fe
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-14 00:48:13 +0200

    ✅ update userController

--- backend/controllers/UserController.py
+++ backend/controllers/UserController.py
@@ -55,25 +55,6 @@ async def changePassword(request: PasswordSchema , payload : dict = Depends(User
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error occurred while saving")
     return JSONResponse(status_code = status.HTTP_200_OK, content = {"message": "Password changed successfully"})
-
-@UserRoutes.get("/getAllUsers" , summary = "Get all signed up users")
-async def getAllUsers(user: dict = Depends(UserServices.is_admin_user)):
-    users =  await User.find().to_list()
-    users = [user.dict() for user in users]
-    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Users retrieved successfully", "users": users})
-
-@UserRoutes.delete("/deleteUser/{id}" , summary = "Delete user by id")
-async def deleteUser(id: str , user: dict = Depends(UserServices.is_admin_user)):
-    user = await User.find_one(User.id == ObjectId(id))
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    await user.delete()
-    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "User deleted successfully"})
-
-@UserRoutes.delete("/deleteAllUsers" , summary = "Delete all users")
-async def deleteAllUsers(user: dict = Depends(UserServices.is_admin_user)):
-    await User.delete_all()
-    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "All users deleted successfully"})
 @UserRoutes.patch("/join_interview/{interview_id}" , summary = "Apply for an interview")
 async def apply_to_interview(interview_id: str , user: dict = Depends(UserServices.is_authorized_user)):
     existed_user = await UserServices.get_user_by_email(user["email"])
