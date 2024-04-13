5c84f7e98c3059eb65c2430182c45e9c15c38b93
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-14 01:48:10 +0200

    🔥 modify company schema

--- backend/schemas/CompanySchema.py
+++ backend/schemas/CompanySchema.py
@@ -6,7 +6,7 @@ class CompanySchema(BaseModel):
     address: str = Field(... , description = "Address of the company" , min_length = 3 , max_length = 50)
     country: str = Field(... , description = "Country of the company" , min_length = 3 , max_length = 50)
     image: Optional[str] = Field(default = "https://www.gravatar.com/avatar/?d=mp" , description = "Image of the company")
-    following_interviewees: Optional[List[str]] = Field(default = [] , description = "List of interviewees")
+    followers: Optional[List[str]] = Field(default = [] , description = "List of interviewees")
     interviews: Optional[List[str]] = Field(default = [] , description = "List of interviews")
     class Config:
         arbitrary_types_allowed = True
