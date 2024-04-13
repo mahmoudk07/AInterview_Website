cbd19b067f90418e4fb06ebe63d1f7a447481ac7
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-13 18:58:15 +0200

    ✅ add interview schema

---
+++ backend/schemas/interviewSchema.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel , Field
+from datetime import date , time
+from typing import List, Optional
+from bson.objectid import ObjectId
+class InterviewSchema(BaseModel):
+    title: str = Field(..., description = "Title of the interview" , min_length = 3 , max_length = 50)
+    status: str = Field(default = "upcoming" , description = "Status of Interview" , enum = ["upcoming" , "finished" , "cancelled"])
+    Date: str = Field(... , description = "Date of the interview")
+    Time: str = Field(... , description = "Time of the interview")
+    questions: List[dict] = Field(... , description = "Questions and answers of the interview")
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: lambda oid: str(oid),  # Converts ObjectId to string for JSON serialization
+    }
+class UpdateInterview(BaseModel):
+    title: Optional[str] = Field(None , description = "Title of the interview" , min_length = 3 , max_length = 50)
+    status: Optional[str] = Field(default = "upcoming" , description = "Status of Interview" , enum = ["upcoming" , "finished" , "cancelled"])
+    Date: Optional[str] = Field(None , description = "Date of the interview")
+    Time: Optional[str] = Field(None ,description = "Time of the interview")
+    questions: Optional[List[dict]] = Field(None ,description = "Questions and answers of the interview")
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: lambda oid: str(oid),  # Converts ObjectId to string for JSON serialization
+        }
