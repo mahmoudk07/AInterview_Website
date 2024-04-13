88a71a457f31b222ee0f4f5d7fa7a67324f78c17
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-14 00:36:04 +0200

    ✅ add company model

---
+++ backend/models/company.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+from beanie import Document, Indexed
+from bson import ObjectId
+
+class Company(Document):
+    name: Indexed(str , unique=True) # type: ignore
+    address: str
+    country: str
+    image: str
+    following_interviewees: List[ObjectId]
+    interviews: List[ObjectId]
+    class Settings:
+        collection = "Company"
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: lambda oid: str(oid),
+        }
+        json_schema_extra = {
+            "example": {
+                "name": "Company",
+                "address": "1234 Main St",
+                "country": "USA",
+                "image": "path/to/image",
+                "following_interviewees": ["5f5f4f7c7b7e13b0f7f3e7b2"],
+                "interviews": ["5f5f4f7c7b7e13b0f7f3e7b2"]
+            }
+        }
