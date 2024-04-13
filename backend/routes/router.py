991579a39a26ad77cc0a113b2353fb820ce64480
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-13 18:58:05 +0200

    ✅ add interview routes

--- backend/routes/router.py
+++ backend/routes/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from controllers.UserController import UserRoutes
-
+from controllers.InterviewController import InterviewRoutes
 router = APIRouter()
 
 router.include_router(UserRoutes , prefix = '/auth' , tags = ["User Authnetication"])
+router.include_router(InterviewRoutes, prefix = '/interview' , tags = ["Interview APIs"])
