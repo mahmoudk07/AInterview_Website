034a0acbe064fc5b7536619fa7ce822c613902dc
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-14 00:36:25 +0200

    ✅ add company controller

---
+++ backend/controllers/CompanyController.py
@@ -0,0 +1,32 @@
+from bson import ObjectId
+from fastapi import APIRouter , Depends , status
+from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+import pymongo.errors
+from models.company import Company
+from schemas.CompanySchema import CompanySchema
+from services.userServices import UserServices
+import pymongo
+CompanyRoutes = APIRouter()
+
+@CompanyRoutes.post('/create' , summary = "Create a new company")
+async def create_company(data : CompanySchema , payload : dict = Depends(UserServices.is_authorized_user)):
+    user = await UserServices.get_user_by_email(payload['email'])
+    newCompany = Company(
+        name = data.name,
+        address = data.address,
+        country = data.country,
+        image = data.image,
+        following_interviewees = data.following_interviewees,
+        interviews = data.interviews
+    )
+    try:
+        await newCompany.create()
+    except pymongo.errors.DuplicateKeyError:
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST , detail = "Company already exists")
+    try:      
+        user.company_id = ObjectId(newCompany.id)
+        await user.save()
+    except Exception as e:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR , detail = "Error occurred while saving")
+    return JSONResponse(status_code = status.HTTP_201_CREATED , content = {"message": "Company created successfully", "company": newCompany.dict()})
