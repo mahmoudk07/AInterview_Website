0dc12c50927ad5a1055c67466ca057b02b539bc5
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-14 17:35:52 +0200

    ✅ add interviews page

---
+++ frontend/src/pages/Company/Interviews/Interviews.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../../../components/Header/Header'
+import Interview from './Components/Interview'
+const Interviews = () => {
+  return (
+    <div className = 'w-full min-h-[80vh] overflow-x-hidden mt-[100px]'>
+        <Header />
+        <div className = 'flex items-center flex-wrap w-full min-h-[100px] px-[5%] py-[2%] gap-x-[2%] gap-y-6'>
+            <Interview />
+            <Interview />
+            <Interview />
+            <Interview />
+        </div>
+    </div>
+  )
+}
+
+export default Interviews
