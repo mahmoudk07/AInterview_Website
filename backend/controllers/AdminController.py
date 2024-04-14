34697407c9b3fd0daa06ece1483250d5941fd8a3
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-15 00:33:31 +0200

    ✅ add more admin apis

--- backend/controllers/AdminController.py
+++ backend/controllers/AdminController.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends , status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
 from models.user import User
+from models.company import Company
 from services.userServices import UserServices
 
 AdminRoutes = APIRouter()
@@ -17,6 +18,11 @@ async def deleteUser(id: str , user: dict = Depends(UserServices.is_admin_user))
     user = await User.find_one(User.id == ObjectId(id))
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if user.role == "manager" and user.company_id != None:
+        company = await Company.find_one(Company.id == user.company_id)
+        if not company:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Company not found")
+        await company.delete()
     await user.delete()
     return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "User deleted successfully"})
 
@@ -24,3 +30,18 @@ async def deleteUser(id: str , user: dict = Depends(UserServices.is_admin_user))
 async def deleteAllUsers(user: dict = Depends(UserServices.is_admin_user)):
     await User.delete_all()
     return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "All users deleted successfully"})
+@AdminRoutes.delete("/deleteCompany/{id}" , summary = "Delete company by id")
+async def delete_company(id : str , payload : dict = Depends(UserServices.is_admin_user)):
+    company = await Company.find_one(Company.id == ObjectId(id))
+    if not company:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Company not found")
+    users_in_company = await User.find(User.company_id == company.id).to_list()
+    for user in users_in_company:
+        user.company_id = None
+        try:
+            await user.save()
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to delete company when trying to remove company from users")
+    await company.delete()
+    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Company deleted successfully"})
+    
