54c73a5e83434f2dcf8552704fe6ea2cb711aac8
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-15 23:48:44 +0200

    ✅ add manager store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import User from './services/auth/authSlice'
+import Manager from "./services/manager/managerSlice"
 export const store = configureStore({
     reducer: {
-        User,
+        User, Manager
     }
 })
