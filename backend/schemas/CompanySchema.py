20cffa07d410cc1228caa4f0a87e180b3a3edd2c
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-15 23:48:52 +0200

    ✅ modify company schema

--- backend/schemas/CompanySchema.py
+++ backend/schemas/CompanySchema.py
@@ -5,6 +5,7 @@ class CompanySchema(BaseModel):
     name: str = Field(... , description = "Name of the company" , min_length = 3 , max_length = 50)
     address: str = Field(... , description = "Address of the company" , min_length = 3 , max_length = 50)
     country: str = Field(... , description = "Country of the company" , min_length = 3 , max_length = 50)
+    website: str = Field(... , description = "Website of the company" , min_length = 3 , max_length = 50)
     image: Optional[str] = Field(default = "https://www.gravatar.com/avatar/?d=mp" , description = "Image of the company")
     followers: Optional[List[str]] = Field(default = [] , description = "List of interviewees")
     interviews: Optional[List[str]] = Field(default = [] , description = "List of interviews")
