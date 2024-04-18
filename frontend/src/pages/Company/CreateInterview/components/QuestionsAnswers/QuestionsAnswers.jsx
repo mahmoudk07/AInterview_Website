162059976e278169adb369c3b5a7c6f9e7d882de
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-18 19:04:02 +0200

    ✅ modify questions and answers

--- frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -6,12 +6,12 @@ const QuestionsAnswers = () => {
     const addQuestion = (e , index) => {
         const newQuestions = [...data.Questions]
         newQuestions[index][`Q${index + 1}`] = e.target.value
-        setData({...data , Questions: newQuestions})
+        setData({...data , questions: newQuestions})
     }
     const addAnswer = (e, index) => {
         const newAnswer = [...data.Questions]
         newAnswer[index]['Answer'] = e.target.value
-        setData({...data , Questions: newAnswer})
+        setData({...data , questions: newAnswer})
     }
   return (
     <div className = 'mt-[60px]'>
@@ -19,7 +19,7 @@ const QuestionsAnswers = () => {
             data.Questions.map((question, index) => (
                 <div key = {index} className='flex-col space-y-2 mb-[20px] bg-transparent p-[3%] border-[2px] border-gray-600 rounded-[10px]'>
                     <Input type="text" label={`Question ${index + 1}`} variant='outlined' color='white' onChange={(e) => addQuestion(e , index)}/>
-                    <Input type="text" label="Answer" variant='outlined' color='white' onChange = {(e) => addAnswer(e, index)}/>
+                    <Input type="text" label="Answer" variant='outlined' color='white' onChange={(e) => addAnswer(e, index)} />
                 </div>
             ))
         }
