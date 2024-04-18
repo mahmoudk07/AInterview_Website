862466137afb0b7d94b4d38932b53597f907d108
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-18 19:04:35 +0200

    ✅ modify interview scheduling

--- frontend/src/pages/Company/CreateInterview/components/Scheduling/InterviewScheduling.jsx
+++ frontend/src/pages/Company/CreateInterview/components/Scheduling/InterviewScheduling.jsx
@@ -1,10 +1,11 @@
 import React, { useContext , useEffect } from 'react'
 import { Input } from '@material-tailwind/react'
 import { InterviewContext } from '../../CreateInterview'
+import "./InterviewScheduling.css"
 const InterviewScheduling = () => {
-  const { data, setData } = useContext(InterviewContext)
+  const { data, setData, numOfQuestions, setNumOfQuestions } = useContext(InterviewContext)
   useEffect(() => {
-    const num = parseInt(data.numOfQuestions);
+    const num = parseInt(numOfQuestions);
     if (num > 0 && Number.isInteger(num)) {
       const newQuestions = Array.from({ length: num }, (_, index) => ({
         [`Q${index + 1}`]: "",
@@ -12,14 +13,15 @@ const InterviewScheduling = () => {
       }));
       setData(prevData => ({ ...prevData, Questions: newQuestions }));
     }
-  }, [data.numOfQuestions , setData]);
+  }, [data.numOfQuestions , setData, numOfQuestions , setNumOfQuestions]);
   return (
     <div className = 'mt-[60px]'>
         <div className = 'flex-col items-center space-y-5'>
             <Input type = "text" label = "Title" variant = 'outlined' color = 'white' onChange = {(e) => setData({...data , title: e.target.value})} value = {data.title}  />
-            <Input type = "text" label = "Date" variant = 'outlined' color = 'white' onChange = {(e) => setData({...data , Date: e.target.value})} value = {data.Date}  />
-            <Input type = "text" label = "Time" variant = 'outlined' color = 'white' onChange = {(e) => setData({...data , Time: e.target.value})} value = {data.Time} />
-            <Input type = "text" label = "Number of Questions" variant = 'outlined' color = 'white' onChange = {(e) => setData({...data , numOfQuestions: e.target.value})} value = {data.numOfQuestions} />
+            <Input type="text" label="Number of Questions" variant='outlined' color='white' onChange={(e) => setNumOfQuestions(e.target.value)} value={numOfQuestions} />
+            <input className = 'date-picker text-white bg-transparent outline-none w-full border-[1px] border-white px-[2%] py-[1.5%] rounded-[10px] cursor-pointer' type = "date" onChange = {(e) => setData({...data , Date: e.target.value})} value = {data.Date}  />
+            <input type="time" className='date-picker text-white bg-transparent outline-none w-full border-[1px] border-white px-[2%] py-[1.5%] rounded-[10px] cursor-pointer' onChange={(e) => setData({ ...data, Time: e.target.value })} value={data.Time} />
+            {/* <Input type = "text" label = "Time" variant = 'outlined' color = 'white' onChange = {(e) => setData({...data , Time: e.target.value})} value = {data.Time} /> */}
         </div>
     </div>
   )
