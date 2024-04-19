e2ac99bef15ce12c641ab3f7cfa2da06d51594cb
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-19 04:25:56 +0200

    ✅ add get company api in company controller

--- backend/controllers/CompanyController.py
+++ backend/controllers/CompanyController.py
@@ -5,7 +5,7 @@ from fastapi.responses import JSONResponse
 import pymongo.errors
 from models.company import Company
 from models.user import User
-from schemas.CompanySchema import CompanySchema
+from schemas.CompanySchema import CompanySchema , updateCompanySchema
 from services.userServices import UserServices
 import pymongo
 
@@ -16,6 +16,14 @@ def extract_specific_fields(user):
         "email": user.email,
         "job": user.job,
         "image": user.image
+    }
+def extract_specific_company_fields(company):
+    return {
+        "name": company.name,
+        "address": company.address,
+        "country": company.country,
+        "website": company.website,
+        "image": company.image,
     }
 CompanyRoutes = APIRouter()
 
@@ -43,6 +51,7 @@ async def create_company(data : CompanySchema , payload : dict = Depends(UserSer
     except Exception as e:
         raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR , detail = "Error occurred while saving")
     return JSONResponse(status_code = status.HTTP_201_CREATED , content = {"message": "Company created successfully", "company": newCompany.dict()})
+
 @CompanyRoutes.get('/followers' , summary = "Get all followers")
 async def get_all_followers(payload : dict = Depends(UserServices.is_authorized_user)):
     user = await UserServices.get_user_by_email(payload['email'])
@@ -56,7 +65,33 @@ async def get_all_followers(payload : dict = Depends(UserServices.is_authorized_
     followers = [extract_specific_fields(user) for user in followers]
     print(followers)
     return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Followers retrieved successfully", "followers": followers})
+
 @CompanyRoutes.get("/get_companies" , summary = "get all companies")
 async def get_all_companies(_ : dict = Depends(UserServices.is_authorized_user)):
     companies = await Company.find().to_list()
     return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Companies retrieved successfully", "companies": companies})
+
+@CompanyRoutes.get('/get_company' , summary = "Get company by id")
+async def get_company_by_id(payload : dict = Depends(UserServices.is_authorized_user)):
+    user = await UserServices.get_user_by_email(payload['email'])
+    company = await Company.find_one(Company.id == user.company_id)
+    if not company:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND , detail = "Company not found")
+    company = extract_specific_company_fields(company)
+    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Company retrieved successfully", "company": company, "user": {"firstname": user.firstname, "lastname": user.lastname, "image": user.image}})
+
+@CompanyRoutes.patch('/update' , summary = "Update company")
+async def update_company(data : updateCompanySchema , payload : dict = Depends(UserServices.is_authorized_user)):
+    user = await UserServices.get_user_by_email(payload['email'])
+    company = await Company.find_one(Company.id == user.company_id)
+    if not company:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND , detail = "Company not found")
+    company.name = data.name
+    company.address = data.address
+    company.country = data.country
+    company.website = data.website
+    try:
+        await company.save()
+    except pymongo.errors.DuplicateKeyError as e:
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST , detail = "Company already exists")
+    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Company updated successfully"})
