5cc8d90d0b6e66c6afeaa60f3185e22f67b8836a
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-19 21:11:23 +0200

    ✨ add updateQuestions Page

---
+++ frontend/src/pages/Company/UpdateInterview/components/UpdateQuestions/UpdateQuestions.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import { UpdateInterviewContext } from '../../UpdateInterview'
+import { Input } from '@material-tailwind/react'
+const UpdateQuestions = () => {
+  const { data, setData, setIsInputChanged } = useContext(UpdateInterviewContext)
+  const addQuestion = (e, index) => {
+    const newQuestions = [...data.questions]
+    newQuestions[index][`Q${index + 1}`] = e.target.value
+    setData({ ...data, questions: newQuestions })
+  }
+  const addAnswer = (e, index) => {
+    const newAnswer = [...data.questions]
+    newAnswer[index]['Answer'] = e.target.value
+    setData({ ...data, questions: newAnswer })
+  }
+  return (
+    <div className='mt-[60px]'>
+      {
+        data?.questions.map((question, index) => (
+          <div key={index} className='flex-col space-y-2 mb-[20px] bg-transparent p-[3%] border-[2px] border-gray-600 rounded-[10px]'>
+            <Input type="text" label={`Question ${index + 1}`} variant='outlined' defaultValue={question[`Q${index + 1}`]} color='white' onChange={(e) => { setIsInputChanged(true); addQuestion(e, index) }} />
+            <Input type="text" label="Answer" variant='outlined' color='white' defaultValue={question.Answer} onChange={(e) => { setIsInputChanged(true); addAnswer(e, index) }} />
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default UpdateQuestions
