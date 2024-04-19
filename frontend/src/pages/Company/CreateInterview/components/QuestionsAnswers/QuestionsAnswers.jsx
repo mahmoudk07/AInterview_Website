26f264ce16b33125193f968d203ede6d3209980a
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-19 21:03:26 +0200

    ✅ modify error

--- frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -4,19 +4,19 @@ import { InterviewContext } from '../../CreateInterview'
 const QuestionsAnswers = () => {
     const { data, setData } = useContext(InterviewContext)
     const addQuestion = (e , index) => {
-        const newQuestions = [...data.Questions]
+        const newQuestions = [...data.questions]
         newQuestions[index][`Q${index + 1}`] = e.target.value
         setData({...data , questions: newQuestions})
     }
     const addAnswer = (e, index) => {
-        const newAnswer = [...data.Questions]
+        const newAnswer = [...data.questions]
         newAnswer[index]['Answer'] = e.target.value
         setData({...data , questions: newAnswer})
     }
   return (
     <div className = 'mt-[60px]'>
         {
-            data.Questions.map((question, index) => (
+            data.questions.map((question, index) => (
                 <div key = {index} className='flex-col space-y-2 mb-[20px] bg-transparent p-[3%] border-[2px] border-gray-600 rounded-[10px]'>
                     <Input type="text" label={`Question ${index + 1}`} variant='outlined' color='white' onChange={(e) => addQuestion(e , index)}/>
                     <Input type="text" label="Answer" variant='outlined' color='white' onChange={(e) => addAnswer(e, index)} />
