9c7dd6a3c2e9cec3dc54ce88e6534db833874d63
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-20 04:42:42 +0200

    ✅ modify navigation route in interview component

--- frontend/src/pages/Company/Interviews/Components/Interview.jsx
+++ frontend/src/pages/Company/Interviews/Components/Interview.jsx
@@ -19,7 +19,7 @@ const Interview = ({ id, title, Date, Time, status, interviewees }) => {
               <span className='text-white font-bold text-[18px]'>Time: <span className='text-gray-400 font-bold'>{Time}</span></span>
               <span className='text-white font-bold text-[18px]'>Status: <span className='text-gray-400'>{status}</span></span>
             <div className = 'flex items-center mt-[5px]'>
-                <div className='cursor-pointer' onClick={() => navigate('/interviewees/1')}>
+                <div className='cursor-pointer' onClick={() => navigate(`/interviewees/${id}`)}>
                     {parseInt(interviewees) !== 0 ? <AvatarStack /> : ''}
                 </div>
                 <span className='text-white text-[14px] ml-[1.5%] font-bold'>{interviewees} Interviewees applied for an interview</span>
