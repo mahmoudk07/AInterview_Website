7df2d5cbb8f98611d5c2173cfa53de65900e1dba
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-20 05:21:40 +0200

    ✅ add scores router

--- backend/routes/router.py
+++ backend/routes/router.py
@@ -3,9 +3,11 @@ from controllers.UserController import UserRoutes
 from controllers.InterviewController import InterviewRoutes
 from controllers.CompanyController import CompanyRoutes
 from controllers.AdminController import AdminRoutes
+from controllers.ScoresController import ScoresRoutes
 router = APIRouter()
 
 router.include_router(UserRoutes , prefix = '/auth' , tags = ["User APIs"])
 router.include_router(InterviewRoutes, prefix = '/interview' , tags = ["Interview APIs"])
 router.include_router(CompanyRoutes, prefix = "/company", tags = ["Company APIs"])
 router.include_router(AdminRoutes, prefix = "/admin", tags = ["Admin APIs"])
+router.include_router(ScoresRoutes, prefix = "/scores", tags = ["Scores APIs"])
