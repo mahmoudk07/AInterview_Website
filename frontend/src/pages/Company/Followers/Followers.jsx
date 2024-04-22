c1fc4174986fd69855830a88c0218622a1b2259c
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-22 20:31:11 +0200

    ✅ add pagninnation to followers page

--- frontend/src/pages/Company/Followers/Followers.jsx
+++ frontend/src/pages/Company/Followers/Followers.jsx
@@ -1,24 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import Header from "../../../components/Header/Header";
-import { Card, Typography, Button, CardBody, CardFooter, Avatar, } from "@material-tailwind/react";
+import { Card, Typography, Button, CardBody, Avatar, IconButton} from "@material-tailwind/react";
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchingFollowers } from '../../../services/manager/managerSlice';
 import { Spinner } from '@material-tailwind/react';
+
 const TABLE_HEAD = ["Member", "Job", "Email"];
 const Followers = () => {
     const { isLoading } = useSelector((state) => state.Manager)
     const dispatch = useDispatch()  
+    const [totalPages, setTotalPages] = useState(null)
     const [data, setData] = useState(null)
+    const [active, setActive] = useState(1);
+    const getItemProps = (index) =>
+    ({
+      variant: active === index ? "filled" : "text",
+      color: "gray",
+      onClick: () => setActive(index),
+    });
+    const next = () => {
+      setActive(active + 1);
+      setData(null)
+    };
+    const prev = () => {
+      setActive(active - 1);
+      setData(null)
+    };
   const handlingFetching = async () => {
-    await dispatch(fetchingFollowers()).then((response) => {
-      if (!response.error)
-          setData(response.payload.followers)
+    await dispatch(fetchingFollowers(active)).then((response) => {
+      if (!response.error) {
+        setData(response.payload.followers)
+        setTotalPages(response.payload.totalPages)
+      }
       })
     }
     useEffect(() => {
       handlingFetching()
         // eslint-disable-next-line
-    }, [])
+    }, [active])
     return (
       <div className="w-full min-h-[80vh] overflow-x-hidden mt-[100px] flex justify-center items-center">
       <Header />
@@ -27,7 +46,7 @@ const Followers = () => {
       </div> : ''}
       {data ?
         <div className="min-w-[50%] mb-[50px]">
-          <Card className="bg-transparent border-[1px] border-borderColor">
+          <Card className="bg-transparent border-[1px] border-borderColor mb-[50px]">
             <div className="w-[100%] text-center mt-[15px] text-[25px] font-bold text-white">
               Followers
             </div>
@@ -102,20 +121,35 @@ const Followers = () => {
               </tbody>
               </table>
             </CardBody>
-            <CardFooter className="flex items-center justify-between border-t border-gray-700 p-4">
-              <Typography variant="small" className="text-white font-bold">
-                Page 1 of 10
-              </Typography>
-              <div className="flex gap-2">
-                <Button variant="outlined" size="sm" className="font-bold text-white border-[2px] border-borderColor">
-                  Previous
-                </Button>
-                <Button variant="outlined" size="sm" className="font-bold text-white border-[2px] border-borderColor">
-                  Next
-                </Button>
+            </Card>
+            <div className={`w-full flex items-center gap-4 ml-[20%] mb-[50px] overflow-x-hidden ${!data ? 'mt-[80vh]' : ''} ${isLoading && data ? 'mt-[80vh]' : ''}`}>
+              <Button
+                variant="text"
+                className="flex items-center gap-2 text-white font-bold border-[1px] border-borderColor text-[14px] "
+                onClick={prev}
+                disabled={active === 1}
+              > Previous
+              </Button>
+              <div className="flex items-center gap-2 flex-wrap">
+                {[...Array(totalPages)].map((_, index) => (
+                  <IconButton
+                    key={index}
+                    className={`text-white bg-transparent border-[1px] border-borderColor text-[16px] font-bold ${active === index + 1 ? "bg-green-700" : ""}`}
+                    {...getItemProps(index + 1)}
+                  >
+                    {index + 1}
+                  </IconButton>
+                ))}
               </div>
-            </CardFooter>
-          </Card>
+              <Button
+                variant="text"
+                className="flex items-center gap-2 text-white font-bold border-[1px] border-borderColor text-[14px]"
+                onClick={next}
+                disabled={active === totalPages}
+              >
+                Next
+              </Button>
+            </div>
         </div> : ''}
     </div>
   )
