fe322fad204e9e34b0ac7657eae8e4f3e3357e4f
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-25 00:09:21 +0200

    ✅ add two nes pages of admin

---
+++ frontend/src/pages/Admin/Companies/Companies.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../../components/Header/Header'
+const Companies = () => {
+  return (
+    <div className='w-full min-h-[80vh] overflow-x-hidden mt-[150px]'>
+        <Header />
+        
+    </div>
+  )
+}
+
+export default Companies
