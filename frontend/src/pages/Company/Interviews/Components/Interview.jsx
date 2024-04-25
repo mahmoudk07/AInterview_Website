d01e594934bdf1871e03f87b3f24347523aac1ee
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-25 04:31:57 +0200

    modify status rendering in interview components

--- frontend/src/pages/Company/Interviews/Components/Interview.jsx
+++ frontend/src/pages/Company/Interviews/Components/Interview.jsx
@@ -9,6 +9,27 @@ const Interview = ({ id, title, Date, Time, status, interviewees }) => {
     const deleteInterview = async () => {
         await axios.delete(`${process.env.REACT_APP_BASE_URL}/interview/delete_interview/${id}`).then((response) => {console.log(response); setShowModal(true) }).catch((error) => console.log(error))
     }
+    const handle_status = (status) => {
+        if (status === "upcoming")
+            return (
+            <>
+                <button className='text-white bg-red-700 px-[3%] py-[1%] rounded-[20px] font-bold' onClick={deleteInterview}>Delete</button>
+                <button className='text-white bg-orange-600 px-[3%] py-[1%] rounded-[20px] font-bold' onClick={() => navigate(`/interview/${id}`)}>Edit</button>
+            </>
+            )
+        else if (status === 'finished')
+            return (
+                <span className = 'font-bold text-orange-800 border-[1px] border-borderColor py-[2%] px-[3%] rounded-[15px] cursor-default'>Still processing!</span>
+            )
+        else if (status === 'current')
+            return (
+                <span className='font-bold text-green-500 border-[1px] border-borderColor py-[2%] px-[3%] rounded-[15px] cursor-default'>Available Now</span>
+            )
+        else
+            return (
+                <button className='text-white bg-orange-600 px-[3%] py-[1%] rounded-[20px] font-bold' onClick={() => navigate(`/result/${id}`)}>Result</button>
+            )
+    }
   return (
     <div className = 'w-full md:w-[32%] h-[280px] bg-red-500 px-[2%] py-[1%] rounded-[20px] bg-transparent border-[1px] border-borderColor cursor-pointer'>
         <div className = 'w-full flex items-center justify-center'>
@@ -17,7 +38,7 @@ const Interview = ({ id, title, Date, Time, status, interviewees }) => {
         <div className = 'flex flex-col justify-center gap-y-2 mt-[10px]'>
               <span className='text-white font-bold text-[18px]'>Date: <span className='text-gray-400 font-bold'>{Date}</span></span>
               <span className='text-white font-bold text-[18px]'>Time: <span className='text-gray-400 font-bold'>{Time}</span></span>
-              <span className='text-white font-bold text-[18px]'>Status: <span className='text-gray-400'>{status}</span></span>
+              <span className='text-white font-bold text-[18px]'>Status: <span className='text-gray-400'>{status.toUpperCase()}</span></span>
             <div className = 'flex items-center mt-[5px]'>
                 <div className='cursor-pointer' onClick={() => navigate(`/interviewees/${id}`)}>
                     {parseInt(interviewees) !== 0 ? <AvatarStack /> : ''}
@@ -25,8 +46,7 @@ const Interview = ({ id, title, Date, Time, status, interviewees }) => {
                 <span className='text-white text-[14px] ml-[1.5%] font-bold'>{interviewees} Interviewees applied for an interview</span>
             </div>
             <div className = 'flex items-center justify-end mt-[15px] gap-5'>
-                <button className = 'text-white bg-red-700 px-[3%] py-[1%] rounded-[20px] font-bold' onClick={deleteInterview}>Delete</button>
-                <button className = 'text-white bg-orange-600 px-[3%] py-[1%] rounded-[20px] font-bold' onClick ={() => navigate(`/interview/${id}`)}>Edit</button>
+                {handle_status(status)}
             </div>
         </div> 
     </div>
