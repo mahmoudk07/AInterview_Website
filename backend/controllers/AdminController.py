3e62e77cd9312fe04ff1763d8bbf157548812480
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-04-25 03:52:14 +0200

    ✅ modify apis in admin controller

--- backend/controllers/AdminController.py
+++ backend/controllers/AdminController.py
@@ -1,17 +1,35 @@
+import asyncio
 from bson import ObjectId
-from fastapi import APIRouter, Depends , status
+from fastapi import APIRouter, Depends, Query , status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
 from models.user import User
 from models.company import Company
+from models.interview import Interview
 from services.userServices import UserServices
-
+from pymongo import DeleteMany
 AdminRoutes = APIRouter()
+
+def extract_users_field(user):
+    return {
+        "id": str(user.id),
+        "firstname": user.firstname,
+        "lastname": user.lastname,
+        "email": user.email,
+        "job": user.job,
+        "image": user.image,
+        "role": user.role,
+    }
+
 @AdminRoutes.get("/getAllUsers" , summary = "Get all signed up users")
-async def getAllUsers(user: dict = Depends(UserServices.is_admin_user)):
-    users =  await User.find().to_list()
-    users = [user.dict() for user in users]
-    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Users retrieved successfully", "users": users})
+async def getAllUsers(page : int = Query(1 , gt=0) , _: dict = Depends(UserServices.is_admin_user)):
+    limit = 6
+    total_count = await User.find({"role" : {"$ne" : "admin"}}).count()
+    totalPages = (total_count + limit - 1) // limit
+    skip = (page - 1)*limit;
+    users =  await User.find({"role" : {"$ne" : "admin"}}).skip(skip).limit(limit).to_list()
+    users = [extract_users_field(user) for user in users]
+    return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Users retrieved successfully", "users": users, "totalPages": totalPages})
 
 @AdminRoutes.delete("/deleteUser/{id}" , summary = "Delete user by id")
 async def deleteUser(id: str , user: dict = Depends(UserServices.is_admin_user)):
@@ -22,7 +40,7 @@ async def deleteUser(id: str , user: dict = Depends(UserServices.is_admin_user))
         company = await Company.find_one(Company.id == user.company_id)
         if not company:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Company not found")
-        await company.delete()
+        await Interview.get_motor_collection().delete_many({'company_id': company.id})
     await user.delete()
     return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "User deleted successfully"})
 
@@ -30,6 +48,7 @@ async def deleteUser(id: str , user: dict = Depends(UserServices.is_admin_user))
 async def deleteAllUsers(user: dict = Depends(UserServices.is_admin_user)):
     await User.delete_all()
     return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "All users deleted successfully"})
+
 @AdminRoutes.delete("/deleteCompany/{id}" , summary = "Delete company by id")
 async def delete_company(id : str , payload : dict = Depends(UserServices.is_admin_user)):
     company = await Company.find_one(Company.id == ObjectId(id))
@@ -42,6 +61,7 @@ async def delete_company(id : str , payload : dict = Depends(UserServices.is_adm
             await user.save()
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to delete company when trying to remove company from users")
+    await Interview.get_motor_collection().delete_many({'company_id': company.id})
     await company.delete()
     return JSONResponse(status_code = status.HTTP_200_OK , content = {"message": "Company deleted successfully"})
     
