b18c51a24358941f0097c50623a932cefa95a07e
ZiadEzzat133 <76906793+ZiadEzzat133@users.noreply.github.com>
2024-05-19 17:56:14 +0300

    Company Card in profile page

---
+++ frontend/src/pages/Interviewers/Company_Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+const Company_Card = ({ comapanyImage, companyname, address }) => {
+    return (
+        <div className='flex flex-row items-center justify-between hover:cursor-pointer'>
+            <div className='flex flex-row space-x-3 items-center '>
+                <div>
+                    <img className="w-10 h-10 rounded-full border-4 border-white" src={comapanyImage} alt="Google" />
+                </div>
+                <div>
+                    <h1 className='text-xl text-bold text-gray-300'>
+                        {companyname}
+                    </h1>
+                    <p className=' text-xs text-gray-400'>
+                        {address}
+                    </p>
+                </div>
+            </div>
+            <div>
+                <button className='text-[15px] font-bold text-white bg-blue-500 outline-none border-none py-[10px] px-[15px] rounded-[20px] transition-all ease-in-out duration-300 hover:bg-blue-500'>Followed</button>
+            </div>
+        </div>
+    )
+}
+
+export default Company_Card
