cc818ddf55ec1ad1b96b7cc18966e4876eee1401
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-05-20 19:17:33 +0300

    ✅ add fields in interview schema

--- backend/schemas/interviewSchema.py
+++ backend/schemas/interviewSchema.py
@@ -3,7 +3,9 @@ from datetime import date , time
 from typing import List, Optional
 from bson.objectid import ObjectId
 class InterviewSchema(BaseModel):
-    title: str = Field(..., description = "Title of the interview" , min_length = 3 , max_length = 50)
+    job_title: str = Field(..., description = "Job Title of the interview" , min_length = 3 , max_length = 50)
+    job_description: str = Field(..., description = "Job Description of the interview" , min_length = 3 , max_length = 500)
+    job_opportunity: str = Field(..., description = "Job Opportunity of the interview" , min_length = 3 , max_length = 100)
     status: str = Field(default = "upcoming" , description = "Status of Interview" , enum = ["upcoming" , "finished" , "current", "processed"])
     Date: str = Field(... , description = "Date of the interview")
     Time: str = Field(... , description = "Time of the interview")
