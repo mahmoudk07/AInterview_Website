9feab37ffb99bae8f17bd5b97d5781c63cfa4a9a
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-05-20 19:56:48 +0300

    ✅ modify update interview page

--- frontend/src/pages/Company/UpdateInterview/components/UpdateScheduling/UpdateScheduling.jsx
+++ frontend/src/pages/Company/UpdateInterview/components/UpdateScheduling/UpdateScheduling.jsx
@@ -7,11 +7,12 @@ const UpdateScheduling = () => {
         <div className='mt-[60px]'>
             {data ? 
                 <div className='flex-col items-center space-y-5'>
-                    <Input type="text" label="Title" variant='outlined' color='white' defaultValue={data?.title} onChange={(e) => { setIsInputChanged(true); setData({ ...data, title: e.target.value }) }} />
+                    <Input type="text" label="Job title" variant='outlined' color='white' defaultValue={data?.job_title} onChange={(e) => { setIsInputChanged(true); setData({ ...data, job_title: e.target.value }) }} />
+                    <Input type="text" label="Job description" variant='outlined' color='white' defaultValue={data?.job_description} onChange={(e) => { setIsInputChanged(true); setData({ ...data, job_description: e.target.value }) }} />
+                    <Input type="text" label="Job opportunity" variant='outlined' color='white' defaultValue={data?.job_opportunity} onChange={(e) => { setIsInputChanged(true); setData({ ...data, job_opportunity: e.target.value }) }} />
                     <Input type="text" label="Number of Questions" variant='outlined' color='white' defaultValue={numOfQuestions} onChange={(e) => { setIsInputChanged(true); setNumOfQuestions(e.target.value) }} />
                     <input className='date-picker text-white bg-transparent outline-none w-full border-[1px] border-white px-[2%] py-[1.5%] rounded-[10px] cursor-pointer' type="date" defaultValue={data?.Date} onChange={(e) => { setIsInputChanged(true); setData({ ...data, Date: e.target.value }) }} />
                     <input type="time" className='date-picker text-white bg-transparent outline-none w-full border-[1px] border-white px-[2%] py-[1.5%] rounded-[10px] cursor-pointer' defaultValue={data?.Time} onChange={(e) => { setIsInputChanged(true); setData({ ...data, Time: e.target.value }) }} />
-                    {/* <Input type = "text" label = "Time" variant = 'outlined' color = 'white' onChange = {(e) => setData({...data , Time: e.target.value})} value = {data.Time} /> */}
                 </div> : ''}
         </div>
   )
