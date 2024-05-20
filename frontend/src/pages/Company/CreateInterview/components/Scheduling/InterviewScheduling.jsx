08eb4fe61ec9909f317b1360ea1bcb27788a501e
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-05-20 22:28:23 +0300

    ✅ add more validations in interview creation

--- frontend/src/pages/Company/CreateInterview/components/Scheduling/InterviewScheduling.jsx
+++ frontend/src/pages/Company/CreateInterview/components/Scheduling/InterviewScheduling.jsx
@@ -6,6 +6,7 @@ import "./InterviewScheduling.css"
 const InterviewScheduling = () => {
   const { data, setData, numOfQuestions, setNumOfQuestions } = useContext(InterviewContext)
   const [showModal, setShowModal] = useState(false)
+  const [isInteger , setIsInteger] = useState(true)
   const closeModal = () => {
     setShowModal(false)
   }
@@ -21,6 +22,20 @@ const InterviewScheduling = () => {
       setShowModal(true)
     }
   };
+  const checkIsNumber = (e) => {
+    const num = parseInt(e)
+    if (num > 0 && Number.isInteger(num)) {
+      setNumOfQuestions(e)
+      setIsInteger(true)
+    }
+    else {
+      setNumOfQuestions("")
+      if (e !== "") {
+        setShowModal(true)
+      }
+      setIsInteger(false)
+    }
+  } 
   useEffect(() => {
     const num = parseInt(numOfQuestions);
     if (num > 0 && Number.isInteger(num)) {
@@ -34,13 +49,13 @@ const InterviewScheduling = () => {
   }, [setData, numOfQuestions , setNumOfQuestions]);
   return (
     <div className='mt-[60px]'>
-        <Modal show={showModal} close={closeModal} message="Please enter a valid Date" />
+      <Modal show={showModal} close={closeModal} message={`${isInteger ? "Please enter a valid date" : "Please enter a valid number of questions" }`} />
         <div className = 'flex-col items-center space-y-5'>
             <Input type="text" label="Job title" variant='outlined' color='white' onChange={(e) => setData({ ...data, job_title: e.target.value })} value={data.job_title} />
             <Input type="text" label="Job Description" variant='outlined' color='white' onChange={(e) => setData({ ...data, job_description: e.target.value })} value={data.job_description} />
             <Input type="text" label="Job Opportunity" variant='outlined' color='white' onChange={(e) => setData({ ...data, job_opportunity: e.target.value })} value={data.job_opportunity} />
-            <Input type="text" label="Number of Questions" variant='outlined' color='white' onChange={(e) => setNumOfQuestions(e.target.value)} value={numOfQuestions} />
-        <input className='date-picker text-white bg-transparent outline-none w-full border-[1px] border-white px-[2%] py-[1.5%] rounded-[10px] cursor-pointer' type="date" onChange={(e) => handleDateChange(e.target.value) } value = {data.Date}  />
+            <Input type="text" label="Number of Questions" variant='outlined' color='white' onChange={(e) => checkIsNumber(e.target.value)} value={numOfQuestions} />
+            <input className='date-picker text-white bg-transparent outline-none w-full border-[1px] border-white px-[2%] py-[1.5%] rounded-[10px] cursor-pointer' type="date" onChange={(e) => handleDateChange(e.target.value) } value = {data.Date}  />
             <input type="time" className='date-picker text-white bg-transparent outline-none w-full border-[1px] border-white px-[2%] py-[1.5%] rounded-[10px] cursor-pointer' onChange={(e) => setData({ ...data, Time: e.target.value })} value={data.Time} />
         </div>
     </div>
