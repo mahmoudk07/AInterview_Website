7aa94bd5d3d470b3b60a4380437b4984aae2be8f
ZiadEzzat133 <76906793+ZiadEzzat133@users.noreply.github.com>
2024-05-20 17:45:25 +0300

    User Profile Finished

--- frontend/src/pages/Interviewers/UserProfile.jsx
+++ frontend/src/pages/Interviewers/UserProfile.jsx
@@ -3,80 +3,15 @@ import InterviewCard from '../../pages/Interviewers/InterviewCard'
 import Header from '../../components/Header/Header'
 import CompanyCard from './CompanyCard'
 import InformationUser from './InformationUser'
-import googleImage from '../../assets/google.jpeg';
-import facebookImage from '../../assets/facebook.jpeg';
-import amazonImage from '../../assets/amazonjpeg.jpeg';
-import microsoftImage from '../../assets/micropng.png';
-import siemensImage from '../../assets/download.png'
 import { useDispatch } from 'react-redux'
 import { getUserInformation } from '../../services/auth/authSlice'
 import { getFollowedCompanies } from '../../services/auth/authSlice'
 import { getFollowedInterviews } from '../../services/auth/authSlice'
-const interviewData = [
-    {
-        image: googleImage,
-        jobTitle: "Google Interview",
-        description: "The job is for a software engineer. The applicant must have at least a bachelor's degree in computer engineering."
-    },
-    {
-        image: facebookImage,
-        jobTitle: "Facebook Interview",
-        description: "The job is for a software engineer. The applicant must have at least a bachelor's degree in computer engineering."
-    },
-    {
-        image: amazonImage,
-        jobTitle: "Amazon Interview",
-        description: "The job is for a software engineer. The applicant must have at least a bachelor's degree in computer engineering."
-    },
-    {
-        image: microsoftImage,
-        jobTitle: "Microsoft Interview",
-        description: "The job is for a software engineer. The applicant must have at least a bachelor's degree in computer engineering."
-    },
-    {
-        image: googleImage,
-        jobTitle: "Google Interview",
-        description: "The job is for a software engineer. The applicant must have at least a bachelor's degree in computer engineering."
-    },
-    {
-        image: siemensImage,
-        jobTitle: "Siemens Interview",
-        description: "The job is for a software engineer. The applicant must have at least a bachelor's degree in computer engineering."
-    }
-];
-
-const companyData = [
-    {
-        comapanyImage: googleImage,
-        companyname: "Google",
-        address: "Mountain View, California"
-    },
-    {
-        comapanyImage: facebookImage,
-        companyname: "Facebook",
-        address: "Menlo Park, California"
-    },
-    {
-        comapanyImage: amazonImage,
-        companyname: "Amazon",
-        address: "Seattle, Washington"
-    },
-    {
-        comapanyImage: microsoftImage,
-        companyname: "Microsoft",
-        address: "Redmond, Washington"
-    },
-    {
-        comapanyImage: siemensImage,
-        companyname: "Siemens",
-        address: "Seattle, Washington"
-    }
-];
 const UserProfile = () => {
     const dispatch = useDispatch()
-    const [userInfo, setUserInfo] = useState(null)
-    const [followed_companies, setFollowedCompanies] = useState(null)
-    const [followed_interviews, setFollowedInterviews] = useState(null)
+    const [userInfo, setUserInfo] = useState([])
+    const [followed_companies, setFollowedCompanies] = useState([])
+    const [followed_interviews, setFollowedInterviews] = useState([])
     const fetchUserInfo = async () => {
         await dispatch(getUserInformation()).then((response) => {
             if (!response.error) {
@@ -111,16 +46,20 @@ const UserProfile = () => {
         <div className = 'w-full min-h-[80vh] mt-[100px] mb-[100px]'>
             <Header />
             <div className=' px-5 py-3 mt-[100px] border border-borderColor rounded-lg w-[80%] m-auto'>
-                <InformationUser />
+                <InformationUser info ={userInfo}  />
                 <div>
                     <h1 className='text-white text-bold text-4xl mt-10 mb-10'>Interviews</h1>
                     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
-                        {interviewData.map((interview, index) => (
+                        {followed_interviews.map((interview, index) => (
                             <InterviewCard
                                 key={index}
                                 image={interview.image}
-                                jobTitle={interview.jobTitle}
-                                description={interview.description}
+                                title={interview.title}
+                                // description={interview.description}
+                                Date={interview.Date}
+                                Time={interview.Time}
+                                company_name={interview.company_name}
+                                status={interview.status}
                             />
                         ))}
                     </div>
@@ -128,11 +67,11 @@ const UserProfile = () => {
                 </div>
                 <h1 className='text-white text-bold text-4xl mt-10 mb-10'>Pages you follow</h1>
                 <div className="mt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-[20px]">
-                {companyData.map((company, index) => (
+                {followed_companies.map((company, index) => (
                         <CompanyCard
                             key={index}
-                            comapanyImage={company.comapanyImage}
-                            companyname={company.companyname}
+                            comapanyImage={company.image}
+                            companyname={company.name}
                             address={company.address}
                         />
                     ))}
