f224ab260d2fa5b1123af7569d5369f772553e72
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-06-07 05:05:26 +0300

    ✅ modify profile page

--- frontend/src/pages/Company/Profile/Profile.jsx
+++ frontend/src/pages/Company/Profile/Profile.jsx
@@ -7,17 +7,32 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchingCompanyInfo } from '../../../services/manager/managerSlice'
 import { fetchingInterviewsStatus } from '../../../services/manager/managerSlice'
 import { Spinner } from '@material-tailwind/react'
+import { initializeApp } from "firebase/app";
+import { ref, uploadBytes, getStorage, getDownloadURL } from 'firebase/storage'
+import { v4 } from 'uuid'
 const Profile = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCmnXe8tNz8Zq04eOgKlvcnvvy_IFg-l6s",
+        authDomain: "ainterview-5e7bf.firebaseapp.com",
+        projectId: "ainterview-5e7bf",
+        storageBucket: "ainterview-5e7bf.appspot.com",
+        messagingSenderId: "579235073668",
+        appId: "1:579235073668:web:06216d7bbb9e9bcf843cd1",
+        measurementId: "G-SZL1XYR7XN"
+    };
+    const app = initializeApp(firebaseConfig);
+    const storage = getStorage(app);
     const { isLoading } = useSelector(state => state.Manager)
     const dispatch = useDispatch()
     const [isInputsChanged, setIsInputChanged] = useState(false)
     const [showModal, setShowModal] = useState(false)
-    const closeModal = () => {
-        setShowModal(false)
-    }
     const [CompanyData, setCompanyData] = useState(null)
     const [userData, setUserData] = useState(null)
     const [interviewsStatus, setInterviewsStatus] = useState(null)
+    const [image, setImage] = useState(userData?.image);
+    const closeModal = () => {
+        setShowModal(false)
+    }
     const handleUpdate = async () => {
         console.log(CompanyData)
         await axios.patch(`${process.env.REACT_APP_BASE_URL}/company/update`, CompanyData, {
@@ -41,6 +56,24 @@ const Profile = () => {
                 setInterviewsStatus(response.payload.results)
         })
     }
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const imageRef = ref(storage, `/images/${file.name + v4()}`);
+            uploadBytes(imageRef, file).then((snapshot) => {
+                getDownloadURL(snapshot.ref).then((url) => {
+                    setImage(url);
+                    setIsInputChanged(true);
+                    setCompanyData({ ...CompanyData, image: url });
+                    setUserData({ ...userData, image: url });
+                }).catch((error) => {
+                    console.error('Error getting download URL:', error);
+                });
+            }).catch((error) => {
+                console.error('Error uploading file:', error);
+            });
+        }
+    };
     useEffect(() => {
         handlingCompanyRequest();
         handlingInterviewsRequest();
@@ -56,11 +89,20 @@ const Profile = () => {
           {CompanyData && userData && interviewsStatus ?
               <div className='flex flex-col lg:flex-row items-center w-[80%] mx-[10%] md:space-x-[5%]'>
                   <div className='flex-col min-h-[400px] w-full md:w-[60%] lg:w-[24%] border-[1px] p-[0%] border-borderColor rounded-[10px] py-[2%] bg-transparent mb-[50px] lg:mb-[0px]'>
-                      <div className='flex-col items-center justify-end text-center mb-[10px]'>
-                          <Avatar className='rounded-[50%] w-[80px] h-[80px] m-auto' src={userData?.image} alt="avatar" />
-                          <span className='block font-bold text-white mt-[8px]'>{userData?.firstname.toUpperCase() + ' ' + userData?.lastname.toUpperCase()}</span>
-                          <span className='block text-[12px] text-gray-300 font-bold'>CEO of {CompanyData?.name}</span>
-                      </div>
+          <div className='flex-col items-center justify-end text-center mb-[10px]'>
+            <div
+                className='relative w-[80px] h-[80px] m-auto'
+            >
+                <Avatar className='rounded-[50%] w-[80px] h-[80px]' src={userData?.image} alt="avatar" />
+                <input
+                    type='file'
+                    className='absolute inset-0 opacity-0 cursor-pointer'
+                    onChange = {handleImageChange}
+                />
+            </div>
+            <span className='block font-bold text-white mt-[8px]'>{userData?.firstname.toUpperCase() + ' ' + userData?.lastname.toUpperCase()}</span>
+            <span className='block text-[12px] text-gray-300 font-bold'>CEO of {CompanyData?.name}</span>
+        </div>
                       <hr className='border-borderColor outline-none b-[20px]' />
                       <div className='flex items-center justify-between p-[5%]'>
                           <span className='text-gray-300'>Finished Interviews</span>
