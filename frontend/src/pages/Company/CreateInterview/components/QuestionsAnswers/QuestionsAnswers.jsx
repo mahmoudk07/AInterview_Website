6da05fdc2ee355d3b8a1ef67e488a08f78c3e1e3
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2024-06-07 17:53:21 +0300

    ✅ fix error in interview creartion page

--- frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ frontend/src/pages/Company/CreateInterview/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -15,7 +15,11 @@ const QuestionsAnswers = () => {
         setData({...data , questions: newAnswer})
     }
     const addType = (e, index) => {
-        if (e.target.value === 'MCQ' || e.target.value === 'TF') { delete data.questions[index]['hint_keywords']; console.log("here") }
+        if (e.target.value === 'MCQ' || e.target.value === 'TF') {
+            if (!data.questions[index].hasOwnProperty('choices'))
+                data.questions[index]['choices'] = ""
+            delete data.questions[index]['hint_keywords'];
+        }
         else {
             data.questions[index]['hint_keywords'] = ""
             if (data.questions[index].hasOwnProperty('choices')) {
@@ -36,6 +40,7 @@ const QuestionsAnswers = () => {
         newChoices[index]['choices'] = e.target.value
         setData({...data , questions: newChoices})
     }
+    // console.log(data)
   return (
     <div className = 'mt-[60px]'>
         {
@@ -43,14 +48,15 @@ const QuestionsAnswers = () => {
                 <div key = {index} className='flex-col space-y-2 mb-[20px] bg-transparent p-[3%] border-[2px] border-gray-600 rounded-[10px]'>
                     <Input type="text" label={`Question ${index + 1}`} variant='outlined' color='white' onChange={(e) => addQuestion(e, index)} />
                     {/* <Input type="text" label="Type" variant='outlined' color='white' onChange={(e) => addType(e, index)} /> */}
-                    <select className = 'bg-transparent text-white border-[1px] border-[white] rounded-[5px] px-[10px] py-[10px] w-full text-[14px] appearance-none outline-none' onChange={(e) => { setQuestionType(e.target.value); addType(e, index); console.log(e.target.value) }}>
+                    <select className='bg-transparent text-white border-[1px] border-[white] rounded-[5px] px-[10px] py-[10px] w-full text-[14px] appearance-none outline-none' onChange={(e) => { setQuestionType(e.target.value); addType(e, index); console.log(e.target.value) }}>
+                        <option>Select Question type</option>
                         <option value="MCQ">MCQ</option>
                         <option value="TF">TF</option>
                         <option value="Technical">Technical</option>
                     </select>
                     <Input type="text" label="Answer" variant='outlined' color='white' onChange={(e) => addAnswer(e, index)} />
-                    {(questionType === 'MCQ' || questionType === 'TF') && <Input type="text" label="Choices" variant='outlined' color='white' onChange={(e) => addChoices(e, index)} />}
-                    <Input type="text" label="Keywords" variant='outlined' color='white' onChange={(e) => addKeywords(e, index)} />
+                    {(data?.questions[index]['Type'] === 'MCQ' || data?.questions[index]['Type'] === 'TF') && <Input type="text" label="Choices" variant='outlined' color='white' onChange={(e) => addChoices(e, index)} />}
+                    {data?.questions[index]["Type"] === 'Technical' && <Input type="text" label="Keywords" variant='outlined' color='white' onChange={(e) => addKeywords(e, index)} />}
                 </div>
             ))
         }
